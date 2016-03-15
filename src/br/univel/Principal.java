afcc521562973f6a570d502364212dc9843b4451
Felix Junior <felixjunior765@gmail.com>
2016-03-15 20:19:55 -0300

    Alteração pedido do cliente.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -12,8 +12,8 @@ public class Principal {
 	
 	public static void main(String[] args){
 		
-		System.out.println("Olá Mundo!");
-		System.out.println("Teste...");
+		System.out.println("Hello Word!");
+		System.out.println("Teste... It's we");
 	}
 	
 }
